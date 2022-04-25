60aa2c533e66b7b6384c158a520a63b6169e108f
nitskp <nitinkpandey4@gmail.com>
2022-04-25 17:48:15 +0530

    Checking Prismjs working or not

--- client/src/components/CustomComponent/CustomComponent.tsx
+++ client/src/components/CustomComponent/CustomComponent.tsx
@@ -1,29 +1,44 @@
 import Prism from "prismjs";
 import { useEffect } from "react";
+import "./prism.css";
 
-interface Idata{
-    content:string,
-
+interface Idata {
+  content: string;
 }
-type propsType={
-    data:Idata|any,
+type propsType = {
+  data: Idata | any;
+};
 
-}
+const CustomComponent = ({ data }: propsType) => {
+  useEffect(() => {
+    setTimeout(() => Prism.highlightAll(), 0);
+  }, []);
 
+  let codeClass: string;
 
-const CustomComponent = ({data}:propsType) => {
-  
-  useEffect(()=>{
-    setTimeout(() => Prism.highlightAll(), 0)
-  },[])
+  switch (data.format) {
+    case "js": {
+      codeClass = "language-javascript";
+      break;
+    }
+    case "css": {
+      codeClass = "language-css";
+      break;
+    }
+    case "html": {
+      codeClass = "language-html";
+      break;
+    }
+    default: {
+      codeClass = "language-react-jsx";
+    }
+  }
 
   return (
     <pre className="line-numbers">
-        <code className="language-css" >
-           {data.content}
-        </code>
+      <code className={codeClass}>{data.content}</code>
     </pre>
-  )
-}
+  );
+};
 
 export default CustomComponent;
