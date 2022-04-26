1e4706aa52b872424e4cd6a9e1a59bbcf4ab2eb4
nitskp <nitinkpandey4@gmail.com>
2022-04-26 16:02:05 +0530

    Added an api for user to get assessments questions

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import slugify from "slugify";
 import admin from "../config/firebaseConfig";
+import { removeCorrectOption } from "../helpers/helperFunction";
 
 const router = express.Router();
 const firestore = admin.firestore();
@@ -17,6 +19,22 @@ router.get("/candidates", async (req, res) => {
     res.status(200).json({ data: data });
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+//GET ASSESSMENT DATA WITHOUT CORRECT OPTION
+// NEED TO ADD AUTH FOR USER
+router.get("/:assessment/questions", async (req, res) => {
+  const assessmentId = slugify(req.params.assessment.toLowerCase());
+
+  try {
+    const docRef = firestore.doc(`assessment/${assessmentId}`);
+    const documentSnapShot = await docRef.get();
+    const data = documentSnapShot.data();
+
+    res.json(data);
+  } catch (error) {
+    res.json(error);
   }
 });
 
