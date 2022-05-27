60bfe1b6b9001e776e33b7bf51c927c870f75188
nitskp <nitinkpandey4@gmail.com>
2022-05-27 16:04:17 +0530

    Added elements in styleguide

--- client/src/components/Button/Button.styled.tsx
+++ client/src/components/Button/Button.styled.tsx
@@ -1,16 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const ButtonContainer= styled.button`
-    background-color: black;
-    color: white;
-    padding: 6px 20px;
-    border-radius: 4px;
-    border: none;
-    margin: 0 auto;
-    display: block;
-    font-size: 16px;
-    margin-bottom: 30px;
+const ButtonContainer = styled.button`
+  background: ${({ theme }) => theme.pellete.main};
+  color: white;
+  padding: 6px 20px;
+  border-radius: 4px;
+  border: none;
+  /* margin: 0 auto; */
+  display: block;
+  font-size: 16px;
+  margin-bottom: 30px;
+`;
 
-`
-
-export {ButtonContainer}
+export { ButtonContainer };
